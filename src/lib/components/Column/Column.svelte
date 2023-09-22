8a89015868a694170eefb1165d9f15ae14f77eab
dev-guy <devguy@devguy.com>
2023-09-22 13:40:26 -0700

    Checkpoint

--- src/lib/components/Column/Column.svelte
+++ src/lib/components/Column/Column.svelte
@@ -7,9 +7,7 @@
 
     let bool_show_options = true;
 
-    export let title;
     export let index_col: number;
-    export let cards;
     export let catsList;
     export let theme;
     export let secondary;
@@ -25,8 +23,17 @@
     let dropHere = false;
     $: dropHere = $dragDrop.to.col === index_col;
 
+	let column;
+	$: column = $board.columns[index_col];
+
+    let cards;
+    $: cards = column.cards;
+
     let numCards = 0;
     $: numCards = cards.length + (dropHere && $dragDrop.from.col === index_col ? 0 : Number(dropHere));
+
+	let title;
+	$: title = column.title;
 
     function handleMouseDown(e, index_card:number){
         if(e.target instanceof HTMLButtonElement) return;
